eb66d108591a8d2275de5220c5bb3011a14237e1
Li Ge <lge@ucsd.edu>
2013-10-27 09:01:06 -0700

    Update at Sun Oct 27 09:01:06 PDT 2013

---
+++ app/main.js
@@ -0,0 +1,22 @@
+// var ISEViewport = require( 'ise-viewport' );
+// var randomCubes = require( 'three-random-cubes' );
+var EditorControls = require( 'ise-editor-controls' );
+
+// var viewport = ISEViewport();
+
+// build scene:
+// var cubes = randomCubes().map( function( c ) { viewport.scene.add(c); return c; } );
+
+var bloxorz = require( 'bloxorz.js' );
+var game = new bloxorz.Game();
+game.loadStage( bloxorz.stages[ 0 ] );
+var controls = EditorControls( game.camera, game.container );
+var THREE = require( 'three' );
+
+var scene = game.scene;
+// scene.add( new THREE.FaceNormalsHelper( game.block, 50 ) )
+// scene.add( new THREE.VertexNormalsHelper( game.block, 50 ) )
+
+setInterval( function() {
+  game.render();
+}, 1000/60 )
---
+++ test/test.js
@@ -0,0 +1,11 @@
+
+var lib = require( 'bloxorz.js' );
+var expect = require( 'expect.js' );
+
+describe( 'bloxorz.js', function() {
+
+  it( 'should return true', function() {
+    expect( true ).to.be( true );
+  } );
+
+} );
